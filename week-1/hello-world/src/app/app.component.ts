b6c422f9a5209adc4c35e25a8e6e5a461e08a556
Laura Kendl <lkendl@live.com>
2022-05-26 21:31:10 -0600

    updated code attribution

--- week-1/hello-world/src/app/app.component.ts
+++ week-1/hello-world/src/app/app.component.ts
@@ -1,3 +1,16 @@
+/*
+============================================
+; Title: Exercise 1.3
+; File Name: app.component.ts
+; Author: Professor Krasso
+; Date: 26 May 2022
+; Modified By: Laura Kendl
+; Description: Demonstrates how to build an Angular application.
+; References:
+; Blackboard: WEB-425 Exercise 1.3 video
+===========================================
+*/
+
 import { Component } from '@angular/core';
 
 @Component({
--- week-1/hello-world/src/app/app.module.ts
+++ week-1/hello-world/src/app/app.module.ts
@@ -1,3 +1,16 @@
+/*
+============================================
+; Title: Exercise 1.3
+; File Name: app.module.ts
+; Author: Professor Krasso
+; Date: 26 May 2022
+; Modified By: Laura Kendl
+; Description: Demonstrates how to build an Angular application.
+; References:
+; Blackboard: WEB-425 Exercise 1.3 video
+===========================================
+*/
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
