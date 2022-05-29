1e2a8bc477857a90247d1d5ebb5d49079ddab115
Laura Kendl <lkendl@live.com>
2022-05-29 09:36:41 -0600

    updated for Assignment 1.5

---
+++ week-1/profile-app/src/app/app.component.ts
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title: Assignment 1.5
+; File Name: app.component.ts
+; Author: Professor Krasso
+; Date: 26 May 2022
+; Modified By: Laura Kendl
+; Description: Demonstrates how to build an Angular application.
+===========================================
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment: string = 'Assignment 1.5 - Components';
+}
