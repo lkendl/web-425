7f729934e307d0741a6f386e47a1c5992be532b8
Laura Kendl <lkendl@live.com>
2022-06-02 22:20:51 -0600

    updated for Exercise 2.3

---
+++ week-2/enhanced-profile-app/src/app/app.component.ts
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title: Exercise 2.3
+; File Name: app.component.ts
+; Author: Professor Krasso
+; Date: 2 June 2022
+; Modified By: Laura Kendl
+; Description: Demonstrates how to build an Angular application.
+===========================================
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  isLoggedIn: Boolean = true;
+  assignment: string = 'Exercise 2.3 - Data Binding';
+}
