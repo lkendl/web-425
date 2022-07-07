710fe4658e4fe69fce38ef3fb14b77fcf9eb4c15
Laura Kendl <lkendl@live.com>
2022-07-07 13:38:31 -0600

    added code attributions

--- week-3/secure-profile-app/src/app/my-image/my-image.component.ts
+++ week-3/secure-profile-app/src/app/my-image/my-image.component.ts
@@ -1,3 +1,15 @@
+/*
+============================================
+; Title: Exercise 3.3
+; File Name: my-image.component.ts
+; Author: Professor Krasso
+; Date: 7 July 2022
+; Modified By: Laura Kendl
+; Description: Demonstrates how to build an Angular application.
+===========================================
+*/
+
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
